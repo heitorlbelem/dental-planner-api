0a3398e96911792b696806e826a8b8c17514461e
Heitor Belém <heitorlbelem98@gmail.com>
2023-01-25 12:48:41 -0300

    creates address model

---
+++ db/migrate/20230125154030_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string :zip_code
+      t.string :full_name
+      t.string :complement
+      t.string :district
+      t.string :state
+      t.string :city
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_035957) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_154030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "zip_code"
+    t.string "full_name"
+    t.string "complement"
+    t.string "district"
+    t.string "state"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "name"
@@ -20,7 +31,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_035957) do
     t.string "cpf"
     t.string "email"
     t.string "birthdate"
-    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cpf"], name: "index_patients_on_cpf", unique: true
