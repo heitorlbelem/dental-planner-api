95ca32f7d8cb9aeca4c4b6d70dc6628746806449
Heitor Belém <heitorlbelem98@gmail.com>
2023-01-25 13:02:59 -0300

    creates the association in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_154030) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_25_155747) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_154030) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "patient_id", null: false
+    t.index ["patient_id"], name: "index_addresses_on_patient_id"
   end
 
   create_table "patients", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_154030) do
     t.index ["email"], name: "index_patients_on_email", unique: true
   end
 
+  add_foreign_key "addresses", "patients"
 end
