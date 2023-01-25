dc0f2ec0c1ae8478b39c88e61a5565dd89f5d8bc
Heitor Belém <heitorlbelem98@gmail.com>
2023-01-25 15:11:44 -0300

    adds spec to validates patients#show request

--- spec/requests/patients_spec.rb
+++ spec/requests/patients_spec.rb
@@ -9,11 +9,44 @@ RSpec.describe 'Patients' do
     before { create_list(:patient, 4) }
 
     it 'returns http status Ok' do
+      do_request
+
       expect(response).to have_http_status(:ok)
     end
 
     it 'returns an array containing all the patients' do
+      do_request
+
       expect(json[:patients].count).to eq(Patient.count)
+    end
+  end
+
+  describe 'GET /patients/:id' do
+    before { create_list(:patient, 4) }
+
+    let(:do_request) { get api_patient_path(id, format: :json) }
+    let(:patient) { Patient.first }
+    let(:id) { patient.id }
+    let(:expected_patient) do
+      {
+        name: patient.name,
+        birthdate: patient.birthdate,
+        cpf: patient.cpf,
+        phone: patient.phone,
+        email: patient.email
+      }
+    end
+
+    it 'returns http status OK' do
+      do_request
+
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a the searched patient with expected attributes' do
+      do_request
+
+      expect(json[:patient]).to eq(expected_patient)
     end
   end
 end
