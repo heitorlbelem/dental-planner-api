c94debde923bd0c5de4057fdcb68dba3d1685b6d
Heitor Belém <heitorlbelem98@gmail.com>
2023-01-25 12:53:17 -0300

    creates specs to validate address attributes

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :address do
+    zip_code { Faker::Address.zip_code }
+    full_name { Faker::Address.full_address }
+    complement { Faker::Address.secondary_address }
+    district { Faker::Address.community }
+    state { Faker::Address.state }
+    city { Faker::Address.city }
+  end
+end
---
+++ spec/models/address_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Address do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:zip_code) }
+    it { is_expected.to validate_presence_of(:full_name) }
+    it { is_expected.to validate_presence_of(:district) }
+    it { is_expected.to validate_presence_of(:state) }
+    it { is_expected.to validate_presence_of(:city) }
+  end
+end
