54937d21ac0557fdfa6852c3351e75f2f2ec30ce
Heitor Belém <heitorlbelem98@gmail.com>
2023-01-30 22:34:15 -0300

    creates users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,14 @@
 
 FactoryBot.define do
   factory :user do
-    # empty user for now
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    username { Faker::Internet.username }
+    email { Faker::Internet.email(name: name) }
+    cpf { Faker::IDNumber.brazilian_citizen_number }
+
+    trait :confirmed_user do
+      confirmed_at { Time.zone.now }
+    end
   end
 end
