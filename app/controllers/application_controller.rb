1b6d08906dc3283e672c4b6d953af4fad96654df
Heitor Belém <heitorlbelem98@gmail.com>
2023-02-01 00:27:53 -0300

    permits user to sign in with username or email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def render_errors(object, status: :unprocessable_entity)
     render 'api/errors', locals: { errors: object.full_messages }, status: status
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,19 @@ class User < ApplicationRecord
   # :timeoutable, :trackable and :omniauthable
   validates :first_name, :last_name, :email, :password, presence: true
   validates :username, uniqueness: true
+  validates :username, format: { with: /^[a-zA-Z0-9_.]*$/, multiline: true }
 
   before_validation :generate_username, on: :create
 
   devise :database_authenticatable, :jwt_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable, :lockable, :confirmable,
     jwt_revocation_strategy: JwtDenylist
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
 
   def generate_username
     return if username.present?
@@ -21,5 +28,17 @@ class User < ApplicationRecord
     end
   end
 
-  def full_name = "#{first_name} #{last_name}"
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(['lower(username) = :value OR lower(email) = :value',
+                                    { value: login.downcase }]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
