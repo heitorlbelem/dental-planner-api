6aedd56c6a444ae69dc87a130fde84bd469f57d9
Heitor Belem <heitorlbelem98@gmail.com>
2024-03-23 11:34:28 -0300

    testando workflow

--- app/controllers/api/restricted/patients/addresses_controller.rb
+++ app/controllers/api/restricted/patients/addresses_controller.rb
@@ -26,6 +26,6 @@ class Api::Restricted::Patients::AddressesController < Api::RestrictedController
   end
 
   def address_params
-    permit_params(only: %i[zip_code street number complement neighborhood state city])
+    permit_params(only: %i[zip_code street number complement neighborhood state city test_rubocop teste_rubocop testando_rubocop])
   end
 end
