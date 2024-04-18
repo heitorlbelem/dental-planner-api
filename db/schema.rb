a37d8b372d34ce4d7c03f04e6e120e60a8724d37
Heitor Belem <heitorlbelem98@gmail.com>
2024-04-18 07:50:52 -0300

    Adiciona referencia a treatment no proceedings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_104036) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_104927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -72,8 +72,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_104036) do
     t.decimal "price", precision: 10, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "treatment_id"
     t.index ["appointment_id"], name: "index_proceedings_on_appointment_id"
     t.index ["patient_id"], name: "index_proceedings_on_patient_id"
+    t.index ["treatment_id"], name: "index_proceedings_on_treatment_id"
   end
 
   create_table "treatments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -116,5 +118,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_104036) do
   add_foreign_key "doctors", "users"
   add_foreign_key "proceedings", "appointments"
   add_foreign_key "proceedings", "patients"
+  add_foreign_key "proceedings", "treatments"
   add_foreign_key "treatments", "patients"
 end
