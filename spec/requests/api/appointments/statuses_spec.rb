21bfe8c3dc7aabe92f288520b879afe57618a161
Heitor Belém <heitorlbelem98@gmail.com>
2024-11-10 01:12:00 -0300

    Cria specs para request de alteraçoes de status

--- spec/requests/api/appointments/statuses_spec.rb
+++ spec/requests/api/appointments/statuses_spec.rb
@@ -34,4 +34,71 @@ RSpec.describe 'Api::Appointments::Statuses' do
       end
     end
   end
+
+  describe 'PATCH /api/appointments/:id/cancel' do
+    let(:do_request) do
+      patch cancel_api_appointment_path(id),
+        headers:,
+        as: :json
+    end
+
+    context 'when appointment can be canceled' do
+      let(:appointment) { create(:appointment) }
+      let(:id) { appointment.id }
+
+      it 'changes the appointment status correctly', :aggregate_failures do
+        do_request
+        expect(response).to have_http_status(:no_content)
+        expect(appointment.reload).to be_canceled
+      end
+    end
+
+    context "when appointment can't be canceled" do
+      let(:appointment) { create(:appointment) }
+      let(:id) { appointment.id }
+
+      before do
+        appointment.confirm!
+        appointment.finish!
+      end
+
+      it 'changes the appointment status correctly', :aggregate_failures do
+        do_request
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(appointment.reload).not_to be_canceled
+      end
+    end
+  end
+
+  describe 'PATCH /api/appointments/:id/finish' do
+    let(:do_request) do
+      patch finish_api_appointment_path(id),
+        headers:,
+        as: :json
+    end
+
+    context 'when appointment can be finished' do
+      let(:appointment) { create(:appointment) }
+      let(:id) { appointment.id }
+
+      before { appointment.confirm! }
+
+      it 'changes the appointment status correctly', :aggregate_failures do
+        do_request
+        expect(response).to have_http_status(:no_content)
+        expect(appointment.reload).to be_finished
+      end
+    end
+
+    context "when appointment can't be finished" do
+      let(:appointment) { create(:appointment) }
+      let(:id) { appointment.id }
+
+      it 'changes the appointment status correctly', :aggregate_failures do
+        do_request
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(appointment.reload).not_to be_finished
+      end
+    end
+  end
 end
