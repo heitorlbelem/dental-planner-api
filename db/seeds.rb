08561e00a7f21efadc215155318be66f85f24139
Heitor Belém <heitorlbelem98@gmail.com>
2024-11-14 00:26:14 -0300

    Cria seed inicial para patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-# TO-DO
+10.times do
+  random_cpf = CPF.generate
+  Patient.create(
+    name: Faker::Name.name,
+    gender: ['female', 'male', 'other'].sample,
+    date_of_birth: Faker::Date.birthday,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.cell_phone,
+    cpf: random_cpf
+  )
+end
