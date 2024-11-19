4c08a895a464274894e851282b7b7f6f7f2a2a97
Heitor Belém <heitorlbelem98@gmail.com>
2024-11-19 17:54:40 -0300

    Cria spec para validar status padrão do bloqueio

--- spec/models/event/block_spec.rb
+++ spec/models/event/block_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Event::Block, type: :model do
+  describe 'callbacks' do
+    context 'when validating status' do
+      it 'set the default value for status' do
+        block = create(:block)
+        expect(block.status).to eq('blocked')
+      end
+    end
+  end
 end
